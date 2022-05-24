c3d320814ed23c46c2e27a632dcc87dfadf9296b
Sabina <alievasabi28@gmail.com>
2022-05-24 03:05:58 +0300

    Firts

---
+++ src/main/java/com/example/mediaplayer/Globals.java
@@ -0,0 +1,8 @@
+package com.example.mediaplayer;
+import javafx.stage.Stage;
+
+
+public class Globals {
+    public static Stage primaryStage;
+    public static int MusicList = 0;
+}
--- src/main/java/com/example/mediaplayer/HelloController.java
+++ src/main/java/com/example/mediaplayer/HelloController.java
@@ -2,23 +2,26 @@ package com.example.mediaplayer;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
-import java.util.*;
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
     public AnchorPane player;
@@ -27,76 +30,32 @@ public class HelloController implements Initializable {
     @FXML
     private Label songLabel;
     @FXML
-    private Button playButton, pauseButton, nextButton, previousButton;
+    private Button playButton, pauseButton, nextButton, previousButton, openButton;
     @FXML
     private Slider volumeSlider;
     @FXML
-    private ProgressBar songProgressBar;
+    private ListView <String> MusicListView;
+    @FXML
+    private Slider progressBar;
+    @FXML
+    private TextField searchBar;
 
 
     private Media media;
     private MediaPlayer mediaPlayer;
+    private List<File> fileList;
+    private final FileChooser fileChooser = new FileChooser();
 
-
-    private File directory;
-    private File[] files;
-
-    private ArrayList<File> songs;
-
-    private int songNumber;
-
-    private Timer timer;
-    private TimerTask task;
-    private boolean running;
+    private int currentSong = 0;
+    private boolean isPlaying = false;
+    private boolean islistOpen = false;
     @FXML
-    private ImageView logoImageView, mainImageView, playlistsImageView;
+    private ImageView logoImageView, mainImageView, playlistsImageView, playImageView;
 
-    @FXML
-    private TableColumn<User, String> artist;
-    @FXML
-    private TextField filterField;
-    @FXML
-    private TableColumn<User, String> name;
-    @FXML
-    private TableView<User> table;
-    @FXML
-    private TableColumn<User, String> time;
-
-    ObservableList<User> list = FXCollections.observableArrayList(
-            new User("I Bet You Look Good On The Dance Floor", "Arctic Monkeys", "2:56"),
-            new User("why'd you only call me when you're high", "Arctic Monkeys", "2:41"),
-            new User("girl crush", "Harry Styles", "4:03"),
-            new User("summertime", "My Chemical Romance", "4:06")
-    );
-    @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        name.setCellValueFactory(new PropertyValueFactory<User, String>("name"));
-        artist.setCellValueFactory(new PropertyValueFactory<User, String>("artist"));
-        time.setCellValueFactory(new PropertyValueFactory<User, String>("time"));
 
-        table.setItems(list);
 
-        songs = new ArrayList<File>();
-        directory = new File("music");
-        files = directory.listFiles();
 
-        if(files != null) {
-            for(File file: files){
-                songs.add(file);
-            }
-        }
-        media = new Media(songs.get(songNumber).toURI().toString());
-        mediaPlayer = new MediaPlayer(media);
-        songLabel.setText(songs.get(songNumber).getName());
-
-        volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
-
-            @Override
-            public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
-                mediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
-            }
-        });
-        songProgressBar.setStyle("-fx-accent:#004cd4");
         File logoFile = new File("images/Component 3.png");
         Image logoImage = new Image(logoFile.toURI().toString());
         logoImageView.setImage(logoImage);
@@ -108,109 +67,240 @@ public class HelloController implements Initializable {
         File playlistsFile = new File("images/Component 5.png");
         Image playlistsImage = new Image(playlistsFile.toURI().toString());
         playlistsImageView.setImage(playlistsImage);
+
+        File playFile = new File("images/Polygon 1.png");
+        Image playImage = new Image(playFile.toURI().toString());
+        playImageView.setImage(playImage);
     }
 
-
-    public void playMedia(){
-        beginTimer();
-        mediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
-        mediaPlayer.play();
-    }
-
-    public void pauseMedia(){
-        endTimer();
-        mediaPlayer.pause();
-    }
-
-    public void nextMedia(){
-        if(songNumber < songs.size() - 1) {
-            songNumber++;
+    public void previousMedia() throws FileNotFoundException {
+        if (mediaPlayer != null && currentSong >0){
             mediaPlayer.stop();
-
-            if(running){
-                endTimer();
-            }
-            media = new Media(songs.get(songNumber).toURI().toString());
+            currentSong--;
+            media = new Media(fileList.get(currentSong).toURI().toASCIIString());
             mediaPlayer = new MediaPlayer(media);
 
-            songLabel.setText(songs.get(songNumber).getName());
-            playMedia();
+            mediaPlayer.play();
+            isPlaying = true;
+            songLabel.setText(fileList.get(currentSong).getName());
+            //playButton.setText("Stop");
+            try {
+                playButton.setGraphic(new ImageView(new Image(new FileInputStream("images/Component 6.png"))));
+            }catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            playVisual();
         }
         else {
-            songNumber = 0;
             mediaPlayer.stop();
-
-            if(running){
-                endTimer();
-            }
-            media = new Media(songs.get(songNumber).toURI().toString());
+            currentSong = fileList.size() -1;
+            media = new Media(fileList.get(currentSong).toURI().toASCIIString());
             mediaPlayer = new MediaPlayer(media);
 
-            songLabel.setText(songs.get(songNumber).getName());
-            playMedia();
+            mediaPlayer.play();
+            isPlaying = true;
+            songLabel.setText(fileList.get(currentSong).getName());
+            try {
+                playButton.setGraphic(new ImageView(new Image(new FileInputStream("images/Component 6.png"))));
+            }catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            playVisual();
         }
     }
 
-    public void previousMedia() {
-
-        if(songNumber > 0) {
-
-            songNumber--;
-
-            mediaPlayer.stop();
-
-            if(running) {
-
-                endTimer();
-            }
-
-            media = new Media(songs.get(songNumber).toURI().toString());
-            mediaPlayer = new MediaPlayer(media);
-
-            songLabel.setText(songs.get(songNumber).getName());
-
-            playMedia();
-        }
-        else {
-
-            songNumber = songs.size() - 1;
-
-            mediaPlayer.stop();
-
-            if(running) {
-
-                endTimer();
-            }
-
-            media = new Media(songs.get(songNumber).toURI().toString());
-            mediaPlayer = new MediaPlayer(media);
-
-            songLabel.setText(songs.get(songNumber).getName());
-
-            playMedia();
-        }
-    }
-
-
-    public void beginTimer(){
-        timer = new Timer();
-        task = new TimerTask() {
-            public void run() {
-                running = true;
-                double current = mediaPlayer.getCurrentTime().toSeconds();
-                double end = media.getDuration().toSeconds();
-                songProgressBar.setProgress(current/end);
-
-                if(current/end == 1){
-                    endTimer();
+    public void playMedia() throws FileNotFoundException {
+        if(mediaPlayer != null) {
+            if (!isPlaying) {
+                mediaPlayer.play();
+                isPlaying = true;
+                try {
+                    playButton.setGraphic(new ImageView(new Image(new FileInputStream("images/Component 6.png"))));
+                }catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                mediaPlayer.pause();
+                isPlaying = false;
+                try {
+                    playButton.setGraphic(new ImageView(new Image(new FileInputStream("images/Polygon 1.png"))));
+                }catch (FileNotFoundException e) {
+                    e.printStackTrace();
                 }
             }
-        };
-        timer.scheduleAtFixedRate(task, 0, 1000);
+        }
+        playVisual();
     }
 
-    public void endTimer(){
-        running = false;
-        timer.cancel();
+    public void nextMedia() throws FileNotFoundException {
+        if(mediaPlayer != null && currentSong < (fileList.size() - 1)) {
+            mediaPlayer.stop();
+            currentSong++;
+            media = new Media(fileList.get(currentSong).toURI().toASCIIString());
+            mediaPlayer = new MediaPlayer(media);
+
+            mediaPlayer.play();
+            isPlaying = true;
+            songLabel.setText(fileList.get(currentSong).getName());
+            try {
+                playButton.setGraphic(new ImageView(new Image(new FileInputStream("images/Component 6.png"))));
+            }catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            playVisual();
+        }
+        else {
+            mediaPlayer.stop();
+            currentSong = 0;
+            media = new Media(fileList.get(currentSong).toURI().toASCIIString());
+            mediaPlayer = new MediaPlayer(media);
+
+            mediaPlayer.play();
+            isPlaying = true;
+            songLabel.setText(fileList.get(currentSong).getName());
+            try {
+                playButton.setGraphic(new ImageView(new Image(new FileInputStream("images/Component 6.png"))));
+            }catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            playVisual();
+        }
+    }
+
+    public void OpenButtonAction() throws FileNotFoundException {
+        List<File> fileListOld = fileList;
+        configureFileChooser(fileChooser);
+        fileList = fileChooser.showOpenMultipleDialog(Globals.primaryStage);
+
+        if(fileList != null) {
+            if (isPlaying) {
+                mediaPlayer.stop();
+                isPlaying = false;
+                try {
+                    playButton.setGraphic(new ImageView(new Image(new FileInputStream("images/Polygon 1.png"))));
+                }catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                MusicListView.getItems().clear();
+                MusicListView.getSelectionModel().selectFirst();
+            }
+            for(int i = 0; i < fileList.size(); i++) {
+                MusicListView.getItems().add(fileList.get(i).getName());
+            }
+
+            Globals.MusicList = fileList.size();
+            currentSong = 0;
+            if (fileList.get(currentSong).toURI() != null) {
+                media = new Media(fileList.get(currentSong).toURI().toASCIIString());
+
+
+                mediaPlayer = new MediaPlayer(media);
+                songLabel.setText(fileList.get(currentSong).getName());
+
+                mediaPlayer.setVolume(0.5);
+                volumeSlider.setValue(mediaPlayer.getVolume());
+
+                volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+                    public void changed(ObservableValue<? extends Number> ov,
+                                        Number old_val, Number new_val) {
+                        mediaPlayer.setVolume(volumeSlider.getValue());
+                    }
+                });
+            }
+        } else {
+            fileList = fileListOld;
+        }
+        mediaPlayer.currentTimeProperty().addListener(new ChangeListener<javafx.util.Duration>() {
+            @Override
+            public void changed(ObservableValue<? extends javafx.util.Duration> observable, javafx.util.Duration oldValue, javafx.util.Duration newValue) {
+                progressBar.setValue(newValue.toSeconds());
+            }
+        }
+        );
+
+        progressBar.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                mediaPlayer.seek(javafx.util.Duration.seconds(progressBar.getValue()));
+            }
+        });
+
+        progressBar.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                mediaPlayer.seek(javafx.util.Duration.seconds(progressBar.getValue()));
+            }
+        });
+
+        mediaPlayer.setOnReady(new Runnable() {
+            @Override
+            public void run() {
+                javafx.util.Duration total = media.getDuration();
+                progressBar.setMax(total.toSeconds());
+            }
+        });
+    }
+
+    public void MusicListViewClickAction() throws FileNotFoundException {
+        if (fileList != null) {
+            mediaPlayer.stop();
+            currentSong = MusicListView.getSelectionModel().getSelectedIndex();
+
+            media = new Media(fileList.get(currentSong).toURI().toASCIIString());
+            if (isPlaying) {
+                mediaPlayer.stop();
+                isPlaying = false;
+                try {
+                    playButton.setGraphic(new ImageView(new Image(new FileInputStream("images/Polygon 1.png"))));
+                }catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+            mediaPlayer = new MediaPlayer(media);
+            songLabel.setText(fileList.get(currentSong).getName());
+
+
+            mediaPlayer.play();
+            playVisual();
+            isPlaying = true;
+            try {
+                playButton.setGraphic(new ImageView(new Image(new FileInputStream("images/Component 6.png"))));
+            }catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void configureFileChooser(
+            final FileChooser fileChooser) {
+        fileChooser.setTitle("View songs");
+        fileChooser.setInitialDirectory(
+                new File(System.getProperty("user.home"))
+        );
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("MP3", "*.mp3")
+        );
+    }
+
+    private void playVisual(){
+        if (mediaPlayer != null){
+            songLabel.setText(fileList.get(currentSong).getName());
+
+            mediaPlayer.setOnEndOfMedia(new Runnable() {
+                @Override
+                public void run() {
+                    isPlaying = false;
+                    try {
+                        playButton.setGraphic(new ImageView(new Image(new FileInputStream("images/Polygon 1.png"))));
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+        }
     }
 }
