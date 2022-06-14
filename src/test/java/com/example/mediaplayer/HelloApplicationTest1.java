019354cebad7bf2336e1c0ac0400742abe4fcfec
Sabina <alievasabi28@gmail.com>
2022-06-14 11:07:57 +0300

    changes in readme file

---
+++ src/test/java/com/example/mediaplayer/HelloApplicationTest1.java
@@ -0,0 +1,12 @@
+package com.example.mediaplayer;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloApplicationTest1 {
+    @Test
+    public void start() {
+    }
+    @Test
+    public void handle(){
+    }
+}
---
+++ src/test/java/com/example/mediaplayer/HelloControllerTest2.java
@@ -0,0 +1,36 @@
+package com.example.mediaplayer;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloControllerTest2 {
+    @Test
+    public void switchTohelloView(){
+    }
+    @Test
+    public void switchTohelloView0(){
+    }
+    @Test
+    public void switchToPlaylist1(){
+    }
+    @Test
+    public void initialize(){
+    }
+    @Test
+    public void previousMedia(){
+    }
+    @Test
+    public void playMedia(){
+    }
+    @Test
+    public void nextMedia(){
+    }
+    @Test
+    public void ShowMusicList(){
+    }
+    @Test
+    public void OpenButtonAction(){
+    }
+    @Test
+    public void MusicListViewClickAction(){
+    }
+}
